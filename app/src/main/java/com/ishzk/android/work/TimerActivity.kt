ba45b2a63cb909b40f8745abfe3e7995b29951d0
17043003 <ishizuka533725@gmail.com>
2022-05-09 07:57:18 +0900

    add draw arc as timer frame.

--- app/src/main/java/com/ishzk/android/work/TimerActivity.kt
+++ app/src/main/java/com/ishzk/android/work/TimerActivity.kt
@@ -1,7 +1,13 @@
 package com.ishzk.android.work
 
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 private const val TAG = "TimerActivity"
@@ -9,11 +15,36 @@ private const val TAG = "TimerActivity"
 class TimerActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_timer)
+//        setContentView(R.layout.activity_timer)
+
+        val canvasView = TimerCanvasView(applicationContext)
+        setContentView(canvasView)
 
         val datas = intent.extras
         val hours: Int = datas?.get("hours").toString().toInt()
         val minutes: Int = datas?.get("minutes").toString().toInt()
         Log.v(TAG, "$hours:$minutes")
+    }
+
+    class TimerCanvasView(context: Context): View(context){
+        private val paint: Paint by lazy { Paint() }
+
+        override fun onDraw(canvas: Canvas?) {
+            super.onDraw(canvas)
+
+            val r = 3 * width / 4f
+            val x = width / 8f
+            val y = height / 4f
+            val rect = RectF(x, y, x + r, y + r)
+            drawBaseArc(canvas, rect)
+        }
+
+        private fun drawBaseArc(canvas: Canvas?, rectF: RectF){
+            paint.color = Color.argb(30, 255, 0, 255)
+            paint.strokeWidth = 220f
+            paint.style = Paint.Style.STROKE
+            paint.isAntiAlias = true
+            canvas?.drawArc(rectF, 0f, 360f, false, paint)
+        }
     }
 }
