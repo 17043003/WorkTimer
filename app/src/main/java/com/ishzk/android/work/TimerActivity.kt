28298c880e3c23a5a76eb90c325d63d3d8825670
17043003 <ishizuka533725@gmail.com>
2022-05-10 07:42:09 +0900

    show progress as filling arc.

--- app/src/main/java/com/ishzk/android/work/TimerActivity.kt
+++ app/src/main/java/com/ishzk/android/work/TimerActivity.kt
@@ -8,6 +8,8 @@ import android.graphics.RectF
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.Transformation
 import androidx.appcompat.app.AppCompatActivity
 
 private const val TAG = "TimerActivity"
@@ -15,7 +17,6 @@ private const val TAG = "TimerActivity"
 class TimerActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_timer)
 
         val canvasView = TimerCanvasView(applicationContext)
         setContentView(canvasView)
@@ -24,10 +25,15 @@ class TimerActivity: AppCompatActivity() {
         val hours: Int = datas?.get("hours").toString().toInt()
         val minutes: Int = datas?.get("minutes").toString().toInt()
         Log.v(TAG, "$hours:$minutes")
+
+        val animation = AnimationDraw(canvasView)
+        animation.duration = (hours * 60 + minutes) * 60 * 1000L
+        canvasView.startAnimation(animation)
     }
 
     class TimerCanvasView(context: Context): View(context){
         private val paint: Paint by lazy { Paint() }
+        var arcRate: Float = 0.0f
 
         override fun onDraw(canvas: Canvas?) {
             super.onDraw(canvas)
@@ -37,6 +43,7 @@ class TimerActivity: AppCompatActivity() {
             val y = height / 4f
             val rect = RectF(x, y, x + r, y + r)
             drawBaseArc(canvas, rect)
+            drawTimerArc(canvas, rect, arcRate)
         }
 
         private fun drawBaseArc(canvas: Canvas?, rectF: RectF){
@@ -46,5 +53,27 @@ class TimerActivity: AppCompatActivity() {
             paint.isAntiAlias = true
             canvas?.drawArc(rectF, 0f, 360f, false, paint)
         }
+
+        private fun drawTimerArc(canvas: Canvas?, rectF: RectF, rate: Float){
+            paint.color = Color.argb(255, 255, 0, 255)
+            paint.strokeWidth = 220f
+            paint.style = Paint.Style.STROKE
+            paint.isAntiAlias = true
+            canvas?.drawArc(rectF, 0f, rate * 360f, false, paint)
+            Log.v(TAG, "Rate is $rate")
+        }
+    }
+}
+
+class AnimationDraw: Animation {
+    private var timerView: TimerActivity.TimerCanvasView
+    constructor(view: TimerActivity.TimerCanvasView){
+        timerView = view
+    }
+    override fun applyTransformation(interpolatedTime: Float, t: Transformation?) {
+        super.applyTransformation(interpolatedTime, t)
+
+        timerView.arcRate = interpolatedTime
+        timerView.requestLayout()
     }
 }
