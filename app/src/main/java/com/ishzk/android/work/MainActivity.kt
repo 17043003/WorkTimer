74c8eac9daf620cc11bc40733a2803bd56431210
17043003 <ishizuka533725@gmail.com>
2022-05-12 20:51:41 +0900

    replace hour and minute picker from textview.

--- app/src/main/java/com/ishzk/android/work/MainActivity.kt
+++ app/src/main/java/com/ishzk/android/work/MainActivity.kt
@@ -5,13 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.NumberPicker
 import android.widget.TextView
 
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
-    private val hourText: TextView by lazy { findViewById(R.id.hourTextNumber) }
-    private val minuteText: TextView by lazy { findViewById(R.id.minuteTextNumber) }
+    private val hourPicker: NumberPicker by lazy { findViewById(R.id.hourPicker) }
+    private val minutePicker: NumberPicker by lazy { findViewById(R.id.minutePicker) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,24 +20,37 @@ class MainActivity : AppCompatActivity() {
 
         val timerStartButton: Button = findViewById(R.id.startTimerButton)
         timerStartButton.setOnClickListener{
-            val (hours, minutes) = getWorkingTime() ?: return@setOnClickListener
+            val (hours, minutes) = getWorkingTime()
             val i = Intent(application, TimerActivity::class.java)
             i.putExtra("hours", hours)
             i.putExtra("minutes", minutes)
             startActivity(i)
         }
+
+        initNumberPicker()
     }
 
-    private fun getWorkingTime(): Pair<Int, Int>?{
-        val hoursString = hourText.text.toString()
-        val minutesString = minuteText.text.toString()
-        if(hoursString.isEmpty() || minutesString.isEmpty()) return null
-
-        val hours = hoursString.toInt()
-        val minutes = minutesString.toInt()
+    private fun getWorkingTime(): Pair<Int, Int>{
+        val hours = hourPicker.value
+        val minutes = minutePicker.value
         Log.v(TAG, "hours:$hours")
         Log.v(TAG, "minutes:$minutes")
 
         return hours to minutes
+    }
+
+    private fun initNumberPicker(){
+        val hourPicker: NumberPicker = findViewById(R.id.hourPicker)
+        hourPicker.apply {
+            maxValue = 99
+            minValue = 0
+            value = 0
+        }
+        val minutePicker: NumberPicker = findViewById(R.id.minutePicker)
+        minutePicker.apply {
+            maxValue = 59
+            minValue = 0
+            value = 0
+        }
     }
 }
